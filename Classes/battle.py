fbb8ba70566dfa8acc4ec8c88aa3522d4946ebe3
harwal14 <harvir.dhaliwal@hotmail.com>
2020-12-30 22:18:15 -0800

    Fixed file names.

--- Classes/battle.py
+++ Classes/battle.py
@@ -1,26 +1,26 @@
-import Chicken
-import Ability
+import chicken
+import ability
 import random
 
 
 class Battle:
-    def __init__(self, challenger: Chicken, champion: Chicken):
+    def __init__(self, challenger: chicken, champion: chicken):
         self.challenger = challenger
         self.champion = champion
 
     @staticmethod
-    def is_critical(attacker: Chicken) -> bool:
+    def is_critical(attacker: chicken) -> bool:
         return random.random() <= attacker.luck / 200
 
     @staticmethod
-    def calculate_damage(attacker: Chicken, defender: Chicken, ability: Ability, is_critical: bool) -> int:
+    def calculate_damage(attacker: chicken, defender: chicken, ability: ability, is_critical: bool) -> int:
         modifier = (random.randint(85 + attacker.luck, 100 + attacker.luck) / 100) * (is_critical + 1)
         base = (((2 * attacker.level / 5 + 2) * ability.power * (attacker.strength / defender.defense)) / 50) + 2
         damage = int(base * modifier)
         return damage
 
-    def challenger_attack(self, ability: Ability, is_critical: bool) -> int:
+    def challenger_attack(self, ability: ability, is_critical: bool) -> int:
         return self.calculate_damage(self, self.challenger, self.champion, ability, is_critical)
 
-    def champion_attack(self, ability: Ability, is_critical: bool) -> int:
+    def champion_attack(self, ability: ability, is_critical: bool) -> int:
         return self.calculate_damage(self, self.champion, self.challenger, ability, is_critical)
