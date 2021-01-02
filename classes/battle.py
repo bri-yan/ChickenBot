a052f7c2ae7803baa7c5cc05e929f77ecbba01ae
bri-yan <briyan@outlook.com>
2021-01-01 19:18:15 -0800

    added types

--- classes/battle.py
+++ classes/battle.py
@@ -2,6 +2,13 @@ from classes.chicken import Chicken
 from classes.ability import Ability
 import random
 
+types = {"fire": {"normal": 1.0, "ice": 1.5, "wind": 1.0, "ground": 1.0, "electric": 1.0, "water": 0.5},
+         "ice": {"normal": 1.0, "fire": 0.5, "wind": 1.5, "ground": 1.0, "electric": 1.0, "water": 1.0},
+         "wind": {"normal": 1.0, "fire": 1.0, "ice": 0.5, "ground": 1.5, "electric": 1.0, "water": 1.0},
+         "ground": {"normal": 1.0, "fire": 1.0, "ice": 1.0, "wind": 0.5, "electric": 1.5, "water": 1.0},
+         "electric": {"normal": 1.0, "fire": 1.0, "ice": 1.0, "wind": 1.0, "ground": 0.5, "water": 1.5},
+         "water": {"normal": 1.0, "fire": 1.5, "ice": 1.0, "wind": 1.0, "ground": 1.0, "electric": 0.5}}
+
 
 class Battle:
     def __init__(self, challenger: Chicken, champion: Chicken):
@@ -13,14 +20,14 @@ class Battle:
         return random.random() <= max(1, attacker.luck) / 200
 
     @staticmethod
-    def is_effective(ability: Ability, defender: Chicken) -> bool:
-        return ability.type
+    def effectiveness(ability: Ability, defender: Chicken) -> float:
+        return types[ability.type][defender.type]
 
-    @staticmethod
-    def calculate_damage(attacker: Chicken, defender: Chicken, ability: Ability, is_critical: bool) -> int:
-        modifier = (random.randint(85 + attacker.luck, 100 + attacker.luck) / 100) * (is_critical + 1)
+    def calculate_damage(self, attacker: Chicken, defender: Chicken, ability: Ability, is_critical: bool) -> int:
+        luck = (random.randint(85 + attacker.luck, 100 + attacker.luck) / 100)
+        modifier = (is_critical + 1) * self.effectiveness(ability, defender)
         base = (((2 * attacker.level / 5 + 2) * ability.power * (attacker.strength / defender.defense)) / 50) + 2
-        damage = int(base * modifier)
+        damage = int(base * modifier * luck)
         return damage
 
     def challenger_attack(self, ability: Ability, is_critical: bool) -> int:
